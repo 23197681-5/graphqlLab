4a2b06b320069ae77812972c52a78d2d55480d43
willian <willianlauber@gmail.com>
2021-10-08 15:28:44 -0300

    query mutations scalar types e tudo que há de bom

--- user/datasource/user.js
+++ user/datasource/user.js
@@ -1,13 +1,51 @@
-const { RESTDataSource} = require("apollo-datasource-rest")
+const { RESTDataSource } = require("apollo-datasource-rest")
 
-class UsersAPI extends RESTDataSource{
-    constructor(){
+class UsersAPI extends RESTDataSource {
+    constructor() {
         super()
         this.baseURL = 'http://localhost:3000'
+        this.customResponse = {
+            code: 200,
+            message: "Success"
+        }
     }
 
-    async getUsers(){
-        return this.get('/users')
+    async getUsers() {
+        const users = await this.get('/users')
+        return users.map(async user => ({
+            id: user.id,
+            nome: user.nome,
+            email: user.email,
+            ativo: user.ativo,
+            role: await this.get(`/roles/${user.role}`)
+        }))
+    }
+
+    async getUserById(id) {
+        var user = this.get(`/users/${id}`) //template string
+        user.role = await this.get(`/roles/${user.role}`);
+        return user;
+    }
+    async addUser(user) {
+        const users = await this.get('/users')
+        user.id = user.length + 1;
+        const role = await this.get(`roles?type=${user.role}`)
+        await this.post('users', {...user, role: role[0].id }) //..object = spred
+        return ({...users, role: role[0] })
+    }
+    async updateUser(newsValues) {
+        const role = await this.get(`/roles/${newsValues.user.role}`);
+        await this.put(`users/${newsValues.id}`, {...novosDados.user, role: role[0].id })
+        return ({
+            ...this.customResponse,
+            user: {...newsValues.user },
+            role: role[0]
+        })
+    }
+    async deleteUser(ID) {
+        console.log("id passado", ID)
+        await this.delete(`users/${ID}`);
+        return this.customResponse;
     }
 }
 
--- user/resolvers/userResolvers.js
+++ user/resolvers/userResolvers.js
@@ -1,9 +1,33 @@
+const { GraphQLScalarType } = require('graphql');
+
+
 const userResolvers = {
-    Query:{
-        users: (root, args, { dataSources }) =>  dataSources.usersAPI.getUsers(),
+        RolesTypes: {
+            ESTUDANTE: "ESTUDANTE",
+            DOCENTE: "DOCENTE",
+            COORDENACAO: "DOCENTE"
+        },
+        //pode ser usado apra validar o valor:
+        DateTime: new GraphQLScalarType({
+            name: 'DateTime',
+            description: 'string of date an hour ISO-8601',
+            serialize: (value) => value.toISOString(),
+            parseValue: (value) => new Date(value),
+            parseLiteral: (ast) => new Date(ast.value)
+        }),
+        Query: {
+            users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+            user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+        },
+        Mutation: {
+            addUser: async(root, { user }, { dataSources }) => dataSources.usersAPI.addUser(user),
+            updateUser: async(root, newData, { dataSources }) => dataSources.usersAPI.updateUser(newData),
+            deleteUser: async(root, { id }, { dataSources }) => dataSources.usersAPI.deleteUser(id)
+        } //root = raiz, resolver raiz, args, context(contexto paara trabalhar)
+        //O argumento context é um objeto que a função resolver recebe com informações e dados para resolver a query — por exemplo, como acessar os dados.
+        // a camada dataSource é responsável por se conectar aos endpoints REST através de seus métodos, como .get().
     }
-}
-//args =  id, dados para inserir
-//datasources permissões
-//info representa a arqvore de query e explica para o resolver o que retornar
+    //args =  id, dados para inserir
+    //datasources permissões
+    //info representa a arqvore de query e explica novosDadospara o resolver o que retornar
 module.exports = userResolvers;
